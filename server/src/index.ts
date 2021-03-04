98da6d7a514cb1d61a05b1dedfeabdeed9a7985d
denis-heka <44803580+denis-heka@users.noreply.github.com>
2021-03-04 12:41:02 +0000

    redirect to heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 // rest of the code remains same
 const app = express();
 const PORT = process.env.PORT || 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server 2'));
+app.get('*', (req, res) => res.send(`Express + TypeScript Server 3, ${req.url}`));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
